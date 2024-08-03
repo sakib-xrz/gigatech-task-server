b2d8cf86e25aeaf21be9b65e5e46b2170d29870f
sakib-xrz <sakibxrz21@gmail.com>
2024-08-04 03:26:20 +0600

    update server.js to use async/await for connecting to the database

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ const app = require("./src/app.js");
 const { config } = require("./src/config/config.js");
 const connectDB = require("./src/config/db.js");
 
-const startServer = async () => {
-  // Connect database
-  await connectDB();
+const port = config.port || 8000;
 
-  const port = config.port || 8000;
-
-  app.listen(port, () => {
-    console.log(`🎯 Server listening on port: ${port}`);
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`🎯 Server listening on port: ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to the database", err);
+    process.exit(1);
   });
-};
 
-startServer();
+module.exports = app;
