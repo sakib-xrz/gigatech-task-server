e6628e9d831db95b6b9ee540425cbaceb59bc818
sakib-xrz <sakibxrz21@gmail.com>
2024-08-04 03:39:04 +0600

    Update CORS configuration in app.js

--- src/app.js
+++ src/app.js
@@ -10,7 +10,13 @@ const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
 app.use(morgan("dev"));
 
 // routes
